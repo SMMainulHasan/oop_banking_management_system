fad37bbc8dde338433c90adc1351d43e274686c3
Mainul Hasan <mainulhasan7070@gmail.com>
2023-07-02 18:01:52 +0600

    adding concept file

---
+++ concpt.py
@@ -0,0 +1,24 @@
+"""
+
+Banking Management System
+-------------------------
+
+User functions->
+1. Can create an account.
+2. Can deposit and withdrawal amount. 
+3. Can check available balance.
+3. Can transfer the amount from his account to another user account.
+4. Can check transaction history.
+5. Can take a loan from the bank twice of his total amount..
+
+Note - User can only withdraw and transfer money from his account if he has money in his account.
+If a user is unable to withdraw the amount of money he has deposited in the bank, he will get a message that the bank is bankrupt.
+
+Admin functions->  
+1. Can create an account
+2. Can check the total available balance of the bank.
+3.Can check the total loan amount.
+4. Can on or off the loan feature of the bank.
+
+
+"""
